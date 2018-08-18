eed92cfd8386cfa352a21d0651c123e26d45bdaf
RuohaiGe <geruohai97@outlook.com>
2018-08-17 23:54:10 -0400

    Home page - Proefssor Contact/Prospective Students/Delete Contact Info;Education - Change Order

---
+++ www/Prospective.php
@@ -0,0 +1,63 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<!-- saved from url=(0044)http://msl.cs.uiuc.edu/~lavalle/message.html -->
+<HTML><HEAD><TITLE>A Message for Prospective Students</TITLE>
+<META http-equiv=Content-Type content="text/html; charset=windows-1252">
+<META content="MSHTML 6.00.2800.1276" name=GENERATOR></HEAD>
+<BODY background=prospective_files/clay.gif>
+<H1>
+<CENTER>A Message for Prospective Graduate Students</CENTER></H1>
+<HR>
+Hello,<BR>
+<P>This message is intended primarily for students who are applying to graduate 
+school, or are starting graduate school, and are considering working in my 
+research group. For students who are already here, you can sign up for an 
+advanced course or reading group that I am organizing. Other possibilities are 
+even dropping by and talking with me, or sending e-mail to set up a visit. 
+First, however, please read this message, especially before trying to visit. 
+<P><B>Thanks for your interest:</B> First of all, I would like to thank you for 
+taking the time to look at my web pages. I appreciate your interest in my 
+research and in the activities of our group. Due to increasing internet 
+connectivity and a large interest in my research area, I receive an overwhelming 
+number of e-mail messages from prospective students. Unfortunately, there are 
+too many messages for me to give a response to each one. I regret that I cannot 
+reply to messages that are so important to the people who send them. For this 
+reason, I have decided to write this message to help give some useful 
+information to you. 
+<P><B>Learn about what we do:</B> If you believe that my research area might 
+potentially be interesting to you, please look carefully at my web pages to 
+learn about our research. You might even want to try downloading some of our 
+papers (if you are unable, please let me know). In general, I am more likely to 
+reply to a student who has taken the time to get some idea of what my research 
+involves, especially if they can explain why they would be well-suited to make 
+contributions in our research group.  
+<P><B>My goals for students:</B> I am very excited about the possibilty of 
+working with exceptional M.S./Ph.D. students. I intend to give strong personal 
+guidance that will maximize their chances of building a rewarding and enjoyable 
+career in research. Important goals that I have for students are: <br>
+<ol>
+	<li>the identification of fundamental research problems by developing a thorough 
+understanding of current literature; </li>
+	<li>the development of a strong technical background that enables the proposal of independent and innovative solutions to challenging problems;</li> 
+	<li>the presentation and publication of research in the most prestigious journals and conferences;</li> 
+	<li>the development of personal communication and presentation skills that are needed to maximize visibility in the research community, and increase chances of future success. If you find this emphasis and my research area appealing, I believe our group could provide you with a vibrant and exciting educational experience. </li>
+</ol>
+<P><B>How to possibly squeeze in:</B> Admissions to Carnegie Mellon
+are based on merit, and teaching assistantships or fellowships are
+usually awarded to the admitted students. I would suggest contacting a
+professor whose research interests <i> really </i> match yours, as
+opposed to a blanket mass-emailed statement. I will ignore those.
+Also, try to apply for fellowships so that you can be self-funded; in
+this way, you can come to graduate school, and then spend time trying
+to find the right research group.  At present, my research group is
+pretty much full; however, I am sometimes willing to make an
+exception. The ordinary path into my group is to become a graduate
+student here and then take an upper-level course from me. If there is
+strong mutual interest between me and a student, chances are good she
+or he can start working in my group.
+<P>In any case, good luck with your prospective graduate studies. 
+<P>Best wishes, 
+<P>Howie Choset, Matthew J. Travers(and Steve LaValle, from whom I copied almost all of this) 
+<P>
+<HR>
+</BODY></HTML>
--- www/index.php
+++ www/index.php
@@ -16,6 +16,61 @@
 
 <!-- Content - Add content for your page here
 ============================= -->
+	<style>
+		.card {
+		    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+		    transition: 0.3s;
+		    width: 80%;
+		    margin-left: 7rem;
+		    margin-right: 10rem;
+		    margin-bottom : 8rem;
+		}
+
+		.card:hover {
+		    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+		}
+
+		.card a{
+			text-align:left;
+			color: #000;
+			display: block;
+			position: relative;
+			box-shadow: 0px 3px 8px #ddd;
+			overflow:hidden;
+		}
+		.card a:hover{
+			text-decoration: none;
+			text-align:left;
+			display: block;
+			position: relative;
+			overflow: hidden;
+		}
+
+		.card a:after{
+		  position: absolute;
+		  content: '';
+		  top: 0rem;
+		  left: 0rem;
+		  bottom: 4rem;
+		  right: 0rem;
+		  opacity:0;
+		  border:.5rem solid #ff8080;
+		  border-bottom:none;
+		  z-index: 100;
+		}
+
+		.card a:hover:after{
+			opacity:1;
+			border:.5rem solid #ff8080;
+			border-bottom:none;
+		}
+
+		.span a:hover span{
+			background-color:#ff8080;
+			color:white;
+		}
+		
+</style>
 	<div class="row">
 
 	  <div class="five columns">
@@ -25,14 +80,31 @@
 			and planning.  Often, we look to biology for inspiration and sometimes,
 			we return the favor by providing analysis that models biology.</p>
 			<p><a href="mission.php">Mission Statement</a></p>
-			<p><a href="http://www.cs.cmu.edu/~choset/prospective.html" target="_blank">Prospective
+			<p><a href="prospective.php" target="_blank">Prospective
 			Students &amp; Post-Docs</a></p>
-			<p><strong>Contact:</strong> info@biorobotics.org</p>
-			
+			<div class="four columns">
+				<div class="card">
+					<a href="https://www.ri.cmu.edu/ri-faculty/matthew-j-travers/" target="_blank">
+	  				<img src="images/MatthewTravers.jpg" alt="MatthewTravers" style="width:100%">
+	  					<div class="container">
+	    					<span><b>Matthew Travers</b><span>
+	  					</div>
+	  				</a>
+				</div>
+			</div>
+			<div class="four columns">
+				<div class="card">
+					<a href="https://www.ri.cmu.edu/ri-faculty/howie-choset/" target="_blank">
+	  				<img src="images/HowieChoset.jpg" alt="HowieChoset" style="width:100%">
+	  					<div class="container">
+	    					<span><b>Howie Choset</b><span> 
+	  					</div>
+	  				</a>
+				</div>
+			</div>
 		</div>
-		
 		<div class="seven columns">
-			<div class="video-container">
+			<div class="video-container" style = "margin-top: 10rem">
 				<iframe src="http://www.youtube.com/embed/AMVO6rI5mL4?rel=0&amp;showinfo=0" frameborder="0"></iframe>
 			</div>
 			<figcaption><strong>Latest Video:</strong>First locomotion tests for a new modular walking robot</figcaption>
