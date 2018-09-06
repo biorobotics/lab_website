c0aa784e4bc0bda711ace5592c97e8f0dc7e8369
Nicolas Zevallos-Roberts <nzevallo@unix4.andrew.cmu.edu>
2018-09-06 12:40:24 -0400

    Reverted changes done to medical snake robot

--- www/robots/medSnake.php
+++ www/robots/medSnake.php
@@ -18,58 +18,77 @@
 		<div class="six columns">
 			<h3><b>Medical Snake Robot</b></h3>
 			<p>
-			When compared to conventional procedures, minimally invasive surgeries (MIS) have potential benefits including “reduced-pain”, minimal “blood-loss”, faster healing and reduced tissue disruption. To further improve upon MIS, our group, along with others have been developing highly articulated snake-like robots to provide deeper access to the anatomy with fewer incisions. The articulation allows a surgeon to insert the robot through a single incision or through a natural orifice and navigate through tightly packed volumes without disturbing the surrounding tissues and organs. This capability however, comes at the cost of limited sensory feedback and reduced situational awareness. In addition, the articulation poses challenges in estimation of the shape configuration of the robot, which is necessary to avoid interfering with organs. 
-
-			<br>
-
-			One of the research directions that we are actively pursuing involves using images obtained from a camera at the head of the robot, to perform monocular SLAM. The goal of our Medical SLAM project is to improve the accuracy and efficacy of the surgery through the adoption of mobile robot simultaneous localization and mapping (SLAM) algorithms to the medical field. SLAM is the problem of estimating an environment map with a mobile robot while simultaneously estimating the pose of the robot in the incrementally constructed map. The advantages of using SLAM for surgery are
-			<ol>
-				<li>the potential improvement in accuracy</li>
-				<li>he estimation of dynamic information</li> 
-				<li>the inference of deformation and stiffness</li>
-			</ol>
-			
-			<br>
-
-			We are also actively developing a controller for the robot that can be controlled directly from MATLAB. This would allow more ease of programming and controlling the robot for various tasks. Some example tasks that we plan to pursue include: vision-based control of the robot, motion planning under uncertainty, force-controlled tissue-palpation, etc. There is also work on developing miniature sensors that can be mounted on the robot to improve the robots perception of the environment. These sensors include force sensors, Laser scanners, miniature stereo cameras, etc.
-
-			<br>
-
+			In order to overcome the limitations of currently available assistive technologies for
+			minimally invasive cardiac surgery (MICS), we develop and tested a first prototype based
+			on an innovative approach of a highly articulated robotic probe (HARP). We hypothesize
+			that, for procedures involving epicardial interventions on the beating heart, MICS can be
+			effectively realized with the HARP, entering the pericardial cavity through a subxiphoid
+			port, reaching remote intrapericardial locations on the epicardium without causing
+			hemodynamic and electrophysiologic interference, attaching to the target surface, and
+			delivering therapeutic interventions under the direct control of the surgeon. We strongly
+			believe that HARP's functionality will eventually lead to application such as multiple
+			intrapericardial therapies (e.g. cell transplantation by intramyocardial injection,
+			epicardial ablation, epicardial lead placement for resynchronization, etc). Our broad,
+			long-term objective is to fulfill our group's "epicardial frontier" vision, enabling a
+			growing portfolio of epicardial therapies by developing and testing new dedicated
+			innovative robotic technology. We envision the adoption of HARP-based intrapericardial
+			therapies not only by minimally invasive cardiac surgeons, but also by interventional
+			cardiologists and electrophysiologists.
 			</p>
 			<p>
 			<a href="http://www.medrobotics.com"> Medrobotics website</a>
 			</p>
-
-			<p>
-			<h5> Papers </h5>
-			<ul id="bullets">
-				<li >
-					<p>				
-					<a href="https://ieeexplore.ieee.org/document/4649903/">
-					CHighly Articulated Robotic Probe for Minimally Invasive Surgery.</a>
-					. Proc. 2012 IEEE International Conference on Robotics and Automation (ICRA), May, 2012. 
-					</p>
-				</li>
-				<li >
-					<p>
-					<a href="https://www.ri.cmu.edu/pub_files/pub4/degani_amir_2006_3/degani_amir_2006_3.pdf">
-					Inequality Constrained Kalman Filtering for the Localization and Registration of
-					a Surgical Robot</a>. Proc. of 30th Annual International IEEE EMBS Conference Vancouver, British Columbia, Canada, August 20-24, 2008 
-					</p>
-				</li>
-				<li >
-					<p>
-					<a href="http://biorobotics.ri.cmu.edu/papers/paperUploads/iros11snakeEstimation.pdf">
-					Percutaneous Intrapericardial Interventions Using a Highly Articulated Robotic Probe.</a> Innovations (Philadelphia, Pa.) 1, no. 6 (2006): 335. 
-					</p>
-				</li>
-			</ul>
 		</div>
+		
 		<div  class="six columns">
-			<image src = "images/medSnake/fig1.png" alt="medSnakeF1" width="100%;"
-			style = "padding-top: 8rem">
-			<image src = "images/medSnake/fig2.png" alt="medSnakeF2" width="100%;"
-			style = "padding-top: 12rem">	
+			<div class="video-container">
+				<iframe src="https://www.youtube.com/embed/QqpWGpVGd9c?rel=0&showinfo=0" frameborder="0"></iframe>
+			</div>
+			<div class="grid gallery small">
+				<ul id="demo-test-gallery" class="demo-gallery">
+				
+				<li><a href="images/medSnake/hires/3DArtic0.jpg" data-size="1200x1600"
+					   data-med="images/medSnake/medres/3DArtic0.jpg" data-med-size="1024x1365">
+					<img class="portrait" src="images/medSnake/thumbs/3DArtic0.jpg" alt="3D Articulation"/>
+				</a></li>
+				
+				<li><a href="images/medSnake/hires/3DArtic1.jpg" data-size="1600x1200"
+					   data-med="images/medSnake/medres/3DArtic1.jpg" data-med-size="1024x768">
+					<img src="images/medSnake/thumbs/3DArtic1.jpg" alt="3D Articulation"/>
+				</a></li>
+				
+				<li><a href="images/medSnake/hires/3DArtic2.jpg" data-size="3456x2304" data-med="images/medSnake/medres/3DArtic2.jpg" data-med-size="1024x683">
+						<img src="images/medSnake/thumbs/3DArtic2.jpg" alt="3D Articulation"/>
+				</a></li>
+				
+				<li><a href="images/medSnake/hires/PlasticHeart.jpg" data-size="1600x1200" data-med="images/medSnake/medres/PlasticHeart.jpg" data-med-size="1024x768">
+					<img src="images/medSnake/thumbs/PlasticHeart.jpg" alt="medSnake on plastic heart"/>
+				</a></li>
+				
+				<li><a href="images/medSnake/hires/Surgery1.jpg" data-size="960x720" data-med="images/medSnake/hires/Surgery1.jpg" data-med-size="960x720">
+					<img src="images/medSnake/thumbs/Surgery1.jpg" alt="medSnake in the operating room"/>
+				</a></li>
+				
+				<li><a href="images/medSnake/hires/Surgery2.jpg" data-size="960x720" data-med="images/medSnake/hires/Surgery2.jpg" data-med-size="960x720">
+					<img src="images/medSnake/thumbs/Surgery2.jpg" alt="medSnake in the operating room"/>
+				</a></li>
+				
+				<li><a href="images/medSnake/hires/Surgery3.jpg" data-size="3456x2304" data-med="images/medSnake/medres/Surgery3.jpg" data-med-size="1024x683">
+					<img src="images/medSnake/thumbs/Surgery3.jpg" alt="medSnake in the operating room"/>
+				</a></li>
+				
+				<li><a href="images/medSnake/hires/Surgery4.jpg" data-size="3456x2304" data-med="images/medSnake/medres/Surgery4.jpg" data-med-size="1024x683">
+					<img src="images/medSnake/thumbs/Surgery4.jpg" alt="medSnake in the operating room"/>
+				</a></li>
+				
+				<li class="placeholder"></li>
+				<li class="placeholder"></li>
+				<li class="placeholder"></li>
+				<li class="placeholder"></li>
+				<li class="placeholder"></li>
+
+				</ul>
+			</div>
 		</div>
     </div>
 	
