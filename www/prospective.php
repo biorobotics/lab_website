3017b203c0d54f9c4ae706111f78dc9ea426a0ae
Nico Zevallos <gnastacast@gmail.com>
2020-08-27 10:44:00 -0400

    Added link to prospective student page

--- www/prospective.php
+++ www/prospective.php
@@ -14,7 +14,7 @@ school, or are starting graduate school, and are considering working in my
 research group. For students who are already here, you can sign up for an 
 advanced course or reading group that I am organizing. Other possibilities are 
 even dropping by and talking with me, or sending e-mail to set up a visit. 
-First, however, please read this message, especially before trying to visit. 
+First, however, please read this message, especially before trying to visit.</P>
 <P><B>Thanks for your interest:</B> First of all, I would like to thank you for 
 taking the time to look at my web pages. I appreciate your interest in my 
 research and in the activities of our group. Due to increasing internet 
@@ -23,18 +23,18 @@ number of e-mail messages from prospective students. Unfortunately, there are
 too many messages for me to give a response to each one. I regret that I cannot 
 reply to messages that are so important to the people who send them. For this 
 reason, I have decided to write this message to help give some useful 
-information to you. 
+information to you.</P>
 <P><B>Learn about what we do:</B> If you believe that my research area might 
 potentially be interesting to you, please look carefully at my web pages to 
 learn about our research. You might even want to try downloading some of our 
 papers (if you are unable, please let me know). In general, I am more likely to 
 reply to a student who has taken the time to get some idea of what my research 
 involves, especially if they can explain why they would be well-suited to make 
-contributions in our research group.  
+contributions in our research group.</P>
 <P><B>My goals for students:</B> I am very excited about the possibilty of 
 working with exceptional M.S./Ph.D. students. I intend to give strong personal 
 guidance that will maximize their chances of building a rewarding and enjoyable 
-career in research. Important goals that I have for students are: <br>
+career in research. Important goals that I have for students are: <br></P>
 <ol>
 	<li>the identification of fundamental research problems by developing a thorough 
 understanding of current literature; </li>
@@ -53,11 +53,12 @@ to find the right research group.  At present, my research group is
 pretty much full; however, I am sometimes willing to make an
 exception. The ordinary path into my group is to become a graduate
 student here and then take an upper-level course from me. If there is
-strong mutual interest between me and a student, chances are good she
-or he can start working in my group.
-<P>In any case, good luck with your prospective graduate studies. 
-<P>Best wishes, 
-<P>Howie Choset, Matthew J. Travers(and Steve LaValle, from whom I copied almost all of this) 
-<P>
+strong mutual interest between me and a student, chances are good they
+can start working in my group.</P>
+<P>To see who is a good point of contact for each project, see <a href="https://docs.google.com/spreadsheets/d/1-xaMOsPz3xbh-Wd5gdpYTj8F79FffHFfgFt7Q_Yokzs/edit?usp=sharing">this google sheet</a>.
+<P>In any case, good luck with your prospective graduate studies.</P>
+<P>Best wishes,</P>
+<P>Howie Choset, Matthew J. Travers(and Steve LaValle, from whom I copied almost all of this)</P>
+<P></P>
 <HR>
 </BODY></HTML>
